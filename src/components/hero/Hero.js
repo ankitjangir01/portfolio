d29f2a56b9176c6cdb2abf909292b6c36ffe2a14
ankitjangir01 <ankitjangirnice@gmail.com>
2023-10-08 19:01:02 +0530

    added fixed contact me button

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -13,63 +13,64 @@ import githubIcon from './../../assets/githubIcon.svg'
 import Background from './Background'
 import Projects from '../Projects'
 import Coding from '../Coding'
+import Contact from '../Contact'
 
 const Hero = () => {
     const [desktopWarnOpen, setDesktopWarnOpen] = useState(true);
 
     return (
         <>
-        <div className='bg-[#bf58ff]'>
-            <Alert color="amber" className='sm:hidden text-white absolute z-[1]' open={desktopWarnOpen} onClose={() => setDesktopWarnOpen(false)}>
-                view in desktop for best experience
-            </Alert>
-            <div className='flex h-screen'>
-                <Background />
-                <div className='hero-text flex-auto mx-auto items-center' style={{ 'margin': 'auto' }}>
-                    <motion.div
-                        initial={{
-                            opacity: 0,
-                            y: -100
-                        }}
-                        animate={{
-                            opacity: 1,
-                            y: 0
-                        }}
-                        transition={{
-                            type: 'spring',
-                            stiffness: 120,
-                            originX: 0,
-                            delay: 0.2,
-                            duration: 1
-                        }}
-                        className='text-8xl md:text-9xl font-extrabold my-auto inline-block z-[1] relative'
-                    >
-                        ANKIT JANGIR
-                    </motion.div>
+            <div className='bg-[#bf58ff]'>
+                <Alert color="amber" className='sm:hidden text-white absolute z-[1]' open={desktopWarnOpen} onClose={() => setDesktopWarnOpen(false)}>
+                    view in desktop for best experience
+                </Alert>
+                <div className='flex h-screen'>
+                    <Background />
+                    <div className='hero-text flex-auto mx-auto items-center' style={{ 'margin': 'auto' }}>
+                        <motion.div
+                            initial={{
+                                opacity: 0,
+                                y: -100
+                            }}
+                            animate={{
+                                opacity: 1,
+                                y: 0
+                            }}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 120,
+                                originX: 0,
+                                delay: 0.2,
+                                duration: 1
+                            }}
+                            className='text-8xl md:text-9xl font-extrabold my-auto inline-block z-[1] relative'
+                        >
+                            ANKIT JANGIR
+                        </motion.div>
 
-                    {/* ============== IDE Card + Social Media ========================== */}
-                    <motion.div
-                        initial={{
-                            opacity: 0,
-                            y: 50
-                        }}
-                        animate={{
-                            opacity: 1,
-                            y: 0
-                        }}
-                        transition={{
-                            type: 'spring',
-                            stiffness: 120,
-                            originX: 0,
-                            delay: .5,
-                            duration: 1
-                        }}
-                    >
-                        <div style={{ zIndex: 1, position: 'relative', marginTop: '16px', display: 'inline-block' }}>
-                            <IDECard />
-                        </div>
-                        {/* social media handles======================= */}
-                        {/* <motion.div className='block h-8 mt-4'
+                        {/* ============== IDE Card + Social Media ========================== */}
+                        <motion.div
+                            initial={{
+                                opacity: 0,
+                                y: 50
+                            }}
+                            animate={{
+                                opacity: 1,
+                                y: 0
+                            }}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 120,
+                                originX: 0,
+                                delay: .5,
+                                duration: 1
+                            }}
+                        >
+                            <div style={{ zIndex: 1, position: 'relative', marginTop: '16px', display: 'inline-block' }}>
+                                <IDECard />
+                            </div>
+                            {/* social media handles======================= */}
+                            {/* <motion.div className='block h-8 mt-4'
                             initial={{
                                 opacity: 0,
                                 y: 50
@@ -118,15 +119,16 @@ const Hero = () => {
                                 <img src={githubIcon} />
                             </motion.a>
                         </motion.div> */}
-                    </motion.div>
+                        </motion.div>
+                    </div>
                 </div>
             </div>
-        </div>
             <Education />
             <Projects />
             <Coding />
             <Skills />
-            </>
+            <Contact />
+        </>
     )
 }
 
