b121c22bf69eeb19ef3ec633bf2bbb8aab15fdf3
ankitjangir01 <ankitjangirnice@gmail.com>
2023-10-08 21:35:15 +0530

    enhanced education content to fit in mobile screen

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,50 +3,44 @@ import { motion } from 'framer-motion'
 import nitrrlogo from './../assets/nitrrlogo.png'
 
 const Education = () => {
-    const [eduState, setEduState] = useState("beforeClick");
+    const [open, setOpen] = useState(false);
 
-    const eduVarient = {
-        beforeClick: {
+    const eduVariant = {
+        false: {
             y: [-4, 4, -4],
             transition: {
                 duration: 1.5,
                 repeat: Infinity
             }
         },
-        afterClick: {
+        true: {
             backgroundColor: '#ffffff',
-            width: 'max-content',
-            height: 'fit-content',
+            width: '100%',
             border: 'solid 12px',
+            height: 'max-content'
         }
     }
 
     return (
-        <div className='hero-bar'>
-            <span className=' flex items-center cursor-pointer overflow-hidden p-2'
-                onClick={() => { eduState === "beforeClick" ? setEduState("afterClick") : setEduState("beforeClick") }}
-            >
-                <span>
-                    EDUCATI
-                </span>
-                <motion.span className='w-12 h-12 sm:w-16 sm:h-16 bg-black rounded-full inline self-center overflow-hidden'
-                    variants={eduVarient}
-                    animate={eduState}
+        <div className='hero-bar overflow-hidden'>
+            <span className='flex hover:cursor-pointer' onClick={() => open ? setOpen(false) : setOpen(true)}>
+                EDUCATI
+                <motion.span className="w-12 h-12 sm:w-16 sm:h-16 bg-black rounded-full overflow-hidden"
+                    variants={eduVariant}
+                    animate={open.toString()}
                 >
-                    <motion.div className='flex p-4 sm:p-8'>
-                        <img src={nitrrlogo} style={{ maxHeight: '130px' }} className='hidden md:block'/>
-                        <div className='text-xl font-bold ml-4 text-left'>
+                    <div className='flex flex-col sm:flex-row p-4 m-4'>
+                        <img src={nitrrlogo} className='w-20 h-20 m-4' />
+                        <div className='text-xl font-bold text-left'>
                             National Institute of Technology (NIT) <br />
                             Raipur, India
                             <hr style={{ height: '2px', backgroundColor: '#000000', marginBottom: '8px' }} />
                             <li className='text-base font-normal'>Electronics and Communication Engineering</li>
                             <li className='text-base font-normal'>July 2019 - July 2023</li>
                         </div>
-                    </motion.div>
+                    </div>
                 </motion.span>
-                <span>
-                    N
-                </span>
+                N
             </span>
         </div>
     )
